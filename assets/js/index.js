1704187244c572649840bbf3b25d5ce235444b00
Gj <648417581@qq,com>
2022-06-13 21:00:50 +0800

    首页功能完毕

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+
+    getUserInfo();
+
+    // 获取layer
+    var layer = layui.layer;
+
+    // 退出登录
+    $("#logout").on('click', function () {
+        // 弹出提示
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //  删除localstorage token
+            localStorage.removeItem('token');
+            // 跳转到登录页面
+            location.href='/login.html';
+
+            layer.close(index);
+        });
+    });
+
+    // 获取用户信息
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            headers: {
+                Authorization: localStorage.getItem('token')
+            },
+            success: function (res) {
+                console.log(res);
+                if(res.status !== 0) {
+                    return layer.msg('获取用户信息失败');
+                }
+                // 渲染头像
+                renderAvatar(res.data);
+            }
+        });
+    }
+
+    // 渲染头像
+    function renderAvatar(user) {
+        var name = user.nickname || user.username;
+        // 设置用户名
+        $("#welcome").html('欢迎&nbsp;&nbsp;' + name);
+
+        // 判断用户头像是否存在
+        if (user.user_pic !== null) {
+            // 有头像
+            $(".layui-nav-img").attr('src', user.user_pic).show();
+            // 隐藏文字头像
+            $(".text-avatar").hide();
+        } else {
+            // 没有头像
+            // 隐藏
+            $(".layui-nav-img").hide();
+            // 获取name首字母转大写
+            var first = name[0].toUpperCase();
+            $(".text-avatar").html(first).show();
+        }
+    }
+});
--- assets/js/webAPI.js
+++ assets/js/webAPI.js
@@ -2,4 +2,24 @@
 $.ajaxPrefilter(function(options){
     // 操作请求url,加上请求根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+    // 如果请求url包含/my/,添加请求头
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
+    // 无论请求成功还是失败，都会调用此函数
+    options.complete = function(res){
+        // console.log('complete');
+        console.log(res);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 清空token
+            localStorage.removeItem('token');
+            // 跳转登录页面
+            location.href='/login.html';
+        }
+    }
+    
 });
